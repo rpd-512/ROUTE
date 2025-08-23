f0fbfc023d82c48c14cddbaac723b576a74473f6
rpd-512 <rpd-512@users.noreply.github.com>
2025-08-23 14:40:50 +0530

    Fixed population generation for isolated nodes

--- include/random_utils.h
+++ include/random_utils.h
@@ -6,7 +6,7 @@
 #include <cmath>
 
 #include "network_utils.h"
-
+#include "debug_utils.h"
 random_device rd;  // Seed the random number engine
 mt19937 gen(rd()); // Standard mersenne_twister_engine
 
@@ -27,13 +27,22 @@ vector<vector<int>> initial_population(int population_size, Topology& network) {
     vector<int> chrm(network.num_nodes);
     for (int i = 0; i < population_size; ++i) {
         // Randomly select a sink node to be the first node in the chromosome
-        unsigned int sink_index = randint(network.num_nodes, network.num_nodes + network.num_sinks - 1);
-        unsigned int assign_sink = randint(0, network.num_nodes - 1);
+        unsigned int sink_index;
+        vector<int> incoming;
+        while(incoming.empty()){
+            sink_index = randint(network.num_nodes, network.num_nodes + network.num_sinks - 1);
+            incoming = network.get_incoming_nodes(sink_index);
+        }
+
+        int incom_index = randint(0,incoming.size() - 1);
+        unsigned int assign_sink = incoming[incom_index];
         chrm[assign_sink] = sink_index;
         // Fill the rest of the chromosome with random node indices
         for (size_t j = 0; j < network.num_nodes; ++j) {
             if (j != assign_sink) {
-                chrm[j] = randint(0, network.node_list.size() - 1);
+                vector<int> reachable = network.get_reachable_nodes(j);
+                int reach_index = randint(0, reachable.size() - 1);
+                chrm[j] = reachable[reach_index];
             }
         }
         if(!hasCycle(chrm, network)){
